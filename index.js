92a5112fc8d338b6070e5aa772387979d4eda48b
plutonsites <plutonsites@gmail.com>
2025-05-13 22:28:47 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-document.getElementById("mobil_logotip").innerHTML = "<img src='pluton.png' alt='logotip' width='70'>";
+document.getElementById("mobil_logotip").innerHTML = "<img src='pluton.png' alt='logotip' width='90'>";
